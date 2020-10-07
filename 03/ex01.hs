f22dcd2b131da28642e2c42341d52c3ca2ad2bb0
Yael <yael.iseli@gmail.com>
2020-10-07 20:36:33 +0200

    feat: add ex 1 and 2

---
+++ 03/ex01.hs
@@ -0,0 +1,31 @@
+import Data.Char
+
+{-
+cette fonction prend en paramètre une liste et retourne en résultat la taille
+de la liste
+
+Exemple:
+* [1,2,3] retourne 3
+-}
+myLength :: Num a => [t] -> a
+myLength l = sum [1 | _ <- l]
+
+{-
+cette fonction prend en paramètre un élément (comparable) et une liste 
+et retourne en résultat la même liste mais sans l'élément indiqué
+
+Exemple:
+* 2 [1,2,3] retourne [[1,3]]
+-}
+myDeleteAll :: Eq a => a -> [a] -> [a]
+myDeleteAll e l = [x | x <- l, x /= e ]
+
+{-
+cette fonction prend en paramètre une chaîne de caractères (donc une liste)
+et la rend sous forme de majuscule
+
+Exemple:
+* "toUpperString" retourne "TOUPPERSTRING"
+-}
+toUpperString :: [Char] -> [Char]
+toUpperString l = [toUpper x | x <- l]
--- 03/ex02.hs
+++ 03/ex02.hs
@@ -1,21 +1,51 @@
 
 {-
+cette fonction prend en paramètre une liste et retourne la liste applatie,
+c'est-à-dire sans élément sous forme de liste
+
+Exemple:
+* [[2,3], [4,2,7], [6,9]] retourne la liste [2,3,4,2,7,6,9]
+-}
+flatten :: [[a]] -> [a]
+flatten [] = []
+flatten (l:ls) = [x | x <- l] ++ (flatten ls)
+{-
+cette fonction prend en paramètre une liste et retourne la liste triée
+
+Exemple:
+* [7,13,4,2,7,34,1,9] retourne [1,2,4,7,7,9,13,34]
+-}
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort [l] = [l]
+quicksort (l:ls) = quicksort [x | x <- ls, x < l] ++ l:(quicksort [x | x <- ls, x > l])
+
+{-
+cette fonction prend en paramètre une liste et retourne toutes les
+partitions de cette liste
+
+Exemple:
+* [1,2,3] retourne [[],[3],[2],[2,3],[1],[1,3],[1,2],[1,2,3]]
+-}
+partitions :: [a] -> [[a]]
+partitions [] = [[]]  
+partitions (l:ls) = (partitions ls) ++ [l:x | x <- (partitions ls)]
+
+-- s02
+myDelete :: Eq t => t -> [t] -> [t]
+myDelete _ [] = []
+myDelete e (x:xs) =
+  if (e == x)
+    then xs
+    else x:(myDelete e xs)
+
+{-
 cette fonction prend en paramètre une liste et retourne en résultat la liste
 des permutations des éléments de cette liste.
 
 Exemple:
 * [1,2,3] retourne [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
-
-TODO: Utilisez une liste en compréhension et la fonction myDelete de la série précédente.
-N'utilisez pas de fonctions d'ordre supérieur.
 -}
-permutations :: [a] -> [[a]]
-permutations = undefined
-
--- implementation found: https://stackoverflow.com/a/19034050/3771148
-permutations' :: [a] -> [[a]]
-permutations' [] = [[]]
-permutations' (x:xs) = [bs ++ x:cs | p' <- permutations xs, (bs, cs) <- splits p']
-    where
-        splits [] = [ ([], []) ]
-        splits (y:ys) = ([], y:ys) : [(y:bs, cs) | (bs, cs) <- splits ys]
+permutations :: Eq a => [a] -> [[a]]
+permutations [] = [[]]
+permutations l = [x:xs | x <- l, xs <- permutations (myDelete x l) ]

71d4acded0c6a09d7c0d7543c279f18c5d21c66c
Yael <yael.iseli@gmail.com>
2020-10-09 17:15:06 +0200

    fix: ex2

--- 04/ex02.hs
+++ 04/ex02.hs
@@ -16,15 +16,14 @@ calculatePolynomial1 [(c,d)] v = c * v ^ d
 calculatePolynomial1 ((c,d):ps) v =  c * v ^ d + (calculatePolynomial1 ps v)
 
 -- version avec liste de compréhension
-calculatePolynomial2 :: (Integral b, Num a) => [(a, b)] -> a -> a
 calculatePolynomial2 poly v = sum [ c * v ^ d | (c,d) <- poly]
 
 -- version avec fonction anonyme et fonction d'ordre supérieur
-calculatePolynomial3 :: (Integral b1, Num b2) => ([b2] -> t) -> [(b2, b1)] -> b2 -> t
-calculatePolynomial3 fct poly v =  fct (map (\(c,d) -> c * v ^d) poly)
+calculatePolynomial3 :: (Integral b, Num a) => [(a, b)] -> a -> a
+calculatePolynomial3 poly v =  sum (map (\(c,d) -> c * v ^d) poly)
 
 
 main = do
   print (calculatePolynomial1 [(2,1), (4,2), (-2,4)] 3)
   print (calculatePolynomial2 [(2,1), (4,2), (-2,4)] 3)
-  print (calculatePolynomial3 sum [(2,1), (4,2), (-2,4)] 3)
+  print (calculatePolynomial3 [(2,1), (4,2), (-2,4)] 3)

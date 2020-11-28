0f54038b75a3afa4b6cb547d0782436fa6dfb48b
Alain Schaller <schallerala@gmail.com>
2020-11-28 17:39:17 +0100

    Done exercise 3

--- 11/second/EX03.pl
+++ 11/second/EX03.pl
@@ -4,9 +4,18 @@
 % myMax(X,Y,X) :- X>=Y,!.
 % myMax(X,Y,Y).
 
+myMax(X,Y,X) :- X>=Y, !.
+myMax(_,Y,Y).
+
 % ## Expliquez pourquoi cette implémentation n’est pas correcte. Montrez à l'aide d'un
 % arbre de recherche que pour certaines questions elle retourne un mauvais résultat
 
+:- write("Pour résoudre Z, le prédicat myMax fonctionne comme espéré, par contre, "),
+   write("si nous posons la question avec un Z, les clauses ne sont pas mutuellements "),
+   write("exclusives. Ce qui permettrait d'obtenir `true` pour myMax(8, 1, 1), "),
+   write("par example"), nl.
 
 
 % ## Proposez une correction
+myMax2(X,Y,X) :- X >= Y, !.
+myMax2(X,Y,Y) :- X < Y.
